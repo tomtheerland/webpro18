eab1e2e94e8395fbddc4f538f5f07a8bb7dc108f
Tom Fevang <tomef96@gmail.com>
2018-05-29 21:17:04 +0200

    backup

--- wp-content/themes/bbe-child-starter/main.js
+++ wp-content/themes/bbe-child-starter/main.js
@@ -1,3 +1,12 @@
 console.log("WOOHOO! The 'main.js' file was executed from the child theme folder.");
 
-//Program here the next web application of the year 2017
+/*function setBtnClass() {
+	var element = document.getElementById("1");
+	element.classList.add("btn");
+	element.classList.add("btn-primary");
+};
+
+window.onload = function() {
+	setBtnClass();	
+};*/
+
